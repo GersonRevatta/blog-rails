01729e3dab6a0f519fe34717a4782edbdefad96e
gerson <gersonrevatta@gmail.com>
2018-08-25 10:38:59 -0500

    Fix redirect in update post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to root_path
+      redirect_to @post
     else
       render :edit
     end
